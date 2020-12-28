586c8282d11cc27582528a66563b5cd7ebfa2ff8
Joao Prates <joaoprates@AirdeJoaoPrates.local>
2020-12-28 00:51:07 -0300

    add alertdialog

--- lib/Home.dart
+++ lib/Home.dart
@@ -42,6 +42,43 @@ class _HomeState extends State<Home> {
               //print("item ${ _itens[indice]["title"] }");
 
               return ListTile(
+                onTap: (){
+                  //print("clique com onTap ${indice}");
+                  showDialog(
+                      context: context,
+                      builder: (context){
+                        return AlertDialog(
+                          title: Text( _itens[indice]["titulo"] ),
+                          titlePadding: EdgeInsets.all(20),
+                          titleTextStyle: TextStyle(
+                              fontSize: 20,
+                              color: Colors.orange
+                          ),
+                          content: Text( _itens[indice]["descricao"] ),
+                          actions: <Widget>[
+                            FlatButton(
+                                onPressed: (){
+                                  print("Selecionado sim");
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Sim")
+                            ),
+                            FlatButton(
+                                onPressed: (){
+                                  print("Selecionado nao");
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Não")
+                            )
+                          ],
+                          //backgroundColor: Colors.orange,
+                        );
+                      }
+                  );
+                },
+                /*onLongPress: (){
+                  print("clique com onLongPress");
+                },*/
                 title : Text( _itens[indice]["title"] ),
                 subtitle: Text( _itens[indice]["description"] ),
               );
